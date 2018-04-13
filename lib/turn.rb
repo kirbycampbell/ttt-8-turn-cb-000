57e7559fa27c18b706502a12495302f0b1e51c73
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-13 18:32:40 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,30 @@
+def display_board(board)
+  puts board
+end
+# Is the User's Input on the Board or Taken already?
+def valid_move?(board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)
+     true
+  if board[index].between?(0,9)
+    position_taken?
+  else
+     false
+  end
+end
+#------------------------------------------------------------
+# Below Defines if the position is taken on the board.
+def position_taken?(board, index)
+    if board[index] == " " || board[index] == ""
+      false
+    elsif board[index] == nil
+      false
+    elsif board[index] != " " || board[index] == ""
+      true
+    else
+      false
+    end
+end
+#---------------------------------------------------------------
+def move()
+
+end
